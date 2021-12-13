e41991022492645463f9708f85566d23eb78df3b
Ayuth Mangmesap <ayuth.mang@gmail.com>
2021-12-13 08:12:46 +0000

    test: refactor test cases

--- test/test.js
+++ test/test.js
@@ -1,44 +1,32 @@
-var thaiIdCard = require('../dist/index.min.js');
-test("Case 1: Valid ID Correct pattern", () => {
-    expect(thaiIdCard.verify("1112034563562")).toBe(true);
-});
+const cases = require("jest-in-case");
+const thaiIdCard = require("../dist/index.min.js");
 
-test("Case 2: Invalid ID Wrong pattern", () => {
-    expect(thaiIdCard.verify("1101700230705")).toBe(false);
-});
+cases(
+    "verify: valid ids",
+    ({ id }) => {
+        expect(thaiIdCard.verify(id)).toBe(true);
+    },
+    [{ name: "correct pattern", id: "1112034563562" }]
+);
 
-test("Case 3: Invalid ID Less than 13 character", () => {
-    expect(thaiIdCard.verify("110170023073")).toBe(false);
-});
+cases(
+    "verify: invalid ids",
+    ({ id }) => {
+        expect(thaiIdCard.verify(id)).toBe(false);
+    },
+    [
+        { name: "wrong pattern", id: "1101700230705" },
+        { name: "less than 13 characters", id: "110170023073" },
+        { name: "mixed characters", id: "11017002070d3" },
+        { name: "mixed characters", id: "rytege54fsfsf" },
+        { name: "just 0", id: "0" },
+        { name: "special character", id: "-" },
+        { name: "blank character", id: "" },
+        { name: "null", id: "" },
+        { name: "only text characters", id: "blablabla" },
+    ]
+);
 
-test("Case 4: Invalid ID Mixed character ", () => {
-    expect(thaiIdCard.verify("11017002070d3")).toBe(false);
-});
-
-test("Case 5: Invalid ID Mixed character", () => {
-    expect(thaiIdCard.verify("rytege54fsfsf")).toBe(false);
-});
-
-test("Case 6: Invalid ID Just 0", () => {
-    expect(thaiIdCard.verify("0")).toBe(false);
-});
-
-test("Case 7: Invalid ID Special character", () => {
-    expect(thaiIdCard.verify("-")).toBe(false);
-});
-
-test("Case 8: Invalid ID Blank character", () => {
-    expect(thaiIdCard.verify("")).toBe(false);
-});
-
-test("Case 9: Invalid ID NULL", () => {
-    expect(thaiIdCard.verify(null)).toBe(false);
-});
-
-test("Case 10: Invalid ID Only text character", () => {
-    expect(thaiIdCard.verify("blablabla")).toBe(false);
-});
-
-test("Case 11: Test Generate", () => {
+test("generate and verify", () => {
     expect(thaiIdCard.verify(thaiIdCard.generate())).toBe(true);
 });
